841c475d238b71bbc23d795ba9125f55c3439bd0
They call me Steve <steve@the-steve.com>
2022-03-13 14:52:26 -0600

    updated entry script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,13 @@ if [ "${COMMAND}" == 'startd' ]; then
 elif [ "${COMMAND}" == 'makeconf' ]; then
 	cp /bitcoin/bitcoin.conf /bitcoin/data
 	exit 0
-# cli runs bitcoin-cli
-# tx runs bitcoin-tx
-# util runs bitcoin-util
-# shell can be used for troubleshooting
-elif [ "${COMMAND}" == 'shell' ]; then
+elif [ "${COMMAND}" == 'cli' ]; then
+	/bitcoin/bin/bitcoin-cli --datadir=/bitcoin/data "${OPTS}"
+elif [ "${COMMAND}" == 'tx' ]; then
+	/bitcoin/bin/bitcoin-tx --datadir=/bitcoin/data "${OPTS}"
+elif [ "${COMMAND}" == 'util' ]; then
+	/bitcoin/bin/bitcoin-util --datadir=/bitcoin/data "${OPTS}"
+elif [ "${COMMAND}" == 'shell' ]; then	# Shell can be used to troubleshoot the container without having to exec into it
 	/bin/sh
 else
 	echo "Unknown command ${COMMAND}" 1>&2
